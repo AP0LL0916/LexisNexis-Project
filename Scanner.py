40d99768972ae57523cd9281234281bfd012eabc
AP0LL0916 <116090905+AP0LL0916@users.noreply.github.com>
2023-05-22 17:13:14 +0000

    Add files via upload

--- Scanner.py
+++ Scanner.py
@@ -1,8 +1,6 @@
 import os
 import time
-import pyodbc
 import json
-import csv
 import pandas as pd
 from datetime import datetime
 
@@ -18,7 +16,7 @@ def start():
     URLnum = input("How many repos will you be entering?\n")
     print("-------------------------------------------------------------")
     print("\n\n\n\n\n\n")
-    print("-------------------------------------------------------------")
+   
     
    
 
@@ -37,24 +35,27 @@ def ScanConfig(urls):
         UrlSet.add(URLname)
     print("\n\n\n\n\n\n")
     
-    serverName = input("Name of SQL server for upload (If none type none)\n")
+   
+
     print("-------------------------------------------------------------")
-    print("\n\n\n\n\n\n")
-    
-    print("-------------------------------------------------------------")
-    databaseName = input("Type name of database for upload (If none type none)\n")
-    print("-------------------------------------------------------------")
-    print("\n\n\n\n\n\n")
-    
-    #Takes user input to set timer for automated scanning
-    timer = input("Enter how many seconds the program should wait until next scan. \n")
+    auto = input("Do you want the program to have automated scanning? \n")
     print("-------------------------------------------------------------")
     print()
-    timer = int(timer)
-    #passes timer and set of URLS to scanning function
-    URLScan(UrlSet, timer, serverName, databaseName)
 
-def URLScan(UrlArray, timer, serverName, databaseName):
+    
+    #Takes user input to set timer for automated scanning
+    if auto == "yes" or auto == "Yes" or auto == "YES":
+        print("-------------------------------------------------------------")
+        timer = input("Enter how many seconds the program should wait until next scan. \n")
+        print("-------------------------------------------------------------")
+        print()
+        timer = int(timer)
+    else:
+        timer = "DONE"
+    #passes timer and set of URLS to scanning function
+    URLScan(UrlSet, timer)
+
+def URLScan(UrlArray, timer):
     #Example URL (https://github.com/trufflesecurity/test_keys)
     
     #Scans through all the URLS and outputs results to results.txt in JSON format
@@ -89,9 +90,9 @@ def URLScan(UrlArray, timer, serverName, databaseName):
         os.system(scanReport)
      
    #Passes URL set and timer to timer function
-    CSV_Report(UrlArray, timer, fileName, csvName, serverName, databaseName)
+    CSV_Report(UrlArray, timer, fileName, csvName)
 
-def CSV_Report(UrlArray, timer, fileName, csvName, serverName, databaseName):
+def CSV_Report(UrlArray, timer, fileName, csvName):
 
     #uses panda module inside virtual environment to conver
     #JSON into a CSV file to be uploaded to SQL server.
@@ -101,39 +102,33 @@ def CSV_Report(UrlArray, timer, fileName, csvName, serverName, databaseName):
     df = pd.read_json(fileName)
     df.to_csv(csvName)
 
-    if serverName == "none" or serverName == "None" or serverName == "NONE" or databaseName == "none" or databaseName == "None" or databaseName == "NONE":
-        Timer(UrlArray, timer, serverName, databaseName)
+   
     
-    SQL_Upload(UrlArray, timer, csvName, serverName, databaseName)
+    Timer(UrlArray, timer)
 
 
-def SQL_Upload(UrlArray, timer, csvName, serverName, databaseName):
-
-    #function handling SQL upload
 
 
-    Sdata = pd.read_csv(csvName)
-    SQL_Data = pd.DataFrame(Sdata)
+def Timer(UrlArray, timer):
 
-    conn = pyobc.connect('Driver= {}')
+    if timer == "DONE":
+        print("Scanning completed. \n\n\n")
+        print("Exiting program. \n\n\n")
+        quit()
 
-    print(SQL_Data)
-
-    Timer(UrlArray, timer, serverName, databaseName)
-
-
-def Timer(UrlArray, timer, serverName, databaseName):
     #https://github.com/AP0LL0916/test-keys-private
     #Counts down till next scan and then sends the set of URLs to scanned again looping until user pushes CTRL + C during timer function
     try:
         timer = str(timer)
+        print("-----------------------------------------------------------------------------")
         print("PROGRAM IS NOW GOING TO SLEEP FOR " + timer + " seconds.")
+        print("-----------------------------------------------------------------------------")
         print("TO QUIT OUT PROGRAM PUSH CTRL + C")
         print("-----------------------------------------------------------------------------")
         timer = int(timer)
         time.sleep(timer)
         print()
-        URLScan(UrlArray, timer, serverName, databaseName)
+        URLScan(UrlArray, timer)
 
     except KeyboardInterrupt:
         print("\n CLOSING PYTHON VIRTUAL ENVIRONMENT")
